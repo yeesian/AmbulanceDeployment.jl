2fc90beaa24719170fcfb66e479326d216ca462a
yeesian <ngyeesian@gmail.com>
2017-04-21 14:09:28 -0400

    misc updates
    - [refactor] remove model_name from `simulate_events!` method; moving
    towards returning a new dataframe
    - [refactor] rename `model` to `dispatch`
    - [refactor] use `redirected!` method
    - [bugfix] dispatch_col -> `:dispatch_from`
    - [bugfix] delay_col -> `:delay`
    - [bugfix] calls -> problem.emergency_calls

--- src/simulate.jl
+++ src/simulate.jl
@@ -30,7 +30,8 @@ function call_event!(
     if sum(problem.deployment[problem.coverage[nbhd,:]]) == 0
         verbose && println("no ambulance reachable for call at $nbhd")
     elseif sum(problem.available[problem.coverage[nbhd,:]]) > 0
-        i = ambulance_for(dispatch, id, problem) # assume valid i
+        i = ambulance_for(dispatch, id, problem)
+        @assert i > 0 # assume valid i (enforced by <if> condition)
         update_ambulances!(dispatch, i, -1)
         problem.emergency_calls[id, :dispatch_from] = i
         problem.available[i] -= 1
@@ -79,6 +80,7 @@ function convey_event!(
         end
         minindex
     end
+    @assert h != 0
     redeploy.hospital[amb] = problem.emergency_calls[id, :hospital] = h
     conveytime = 15 + ceil(Int, 60*problem.emergency_calls[id, Symbol("hosp$(h)_min")])
     conveying!(redeploy, amb, h, t)
@@ -103,19 +105,17 @@ end
 function done_event!(
         events::PriorityQueue,
         problem::DispatchProblem,
-        model::DispatchModel,
+        dispatch::DispatchModel,
         redeploy::DeployModel,
         id::Int,
         t::Int,
         amb::Int
     )
     stn = redeploy.assignment[amb]
+    @assert stn > 0
     if sum(length(wq) for wq in problem.wait_queue[problem.coverage[:,stn]]) > 0
         # people are waiting in a queue
-        @assert redeploy.status[amb] == :returning
-        redeploy.fromtime[amb] = t
-        redeploy.status[amb] = :responding
-
+        redirected!(redeploy, amb, t)
         # determine the person who has waited the longest
         minindex = 0; mintime = Inf
         for nbhd in 1:size(problem.coverage,1)
@@ -128,29 +128,29 @@ function done_event!(
             end
         end
         @assert minindex != 0 && t - mintime >= 0 && mintime < Inf
-
         # respond to the person
         let id = shift!(problem.wait_queue[minindex])
             problem.emergency_calls[id, :dispatch_from] = stn
             travel_time = ceil(Int,60*problem.emergency_calls[id, Symbol("stn$(stn)_min")])
             @assert t - mintime >= 0 && travel_time > 0 && t > 0
             total_delay = (t - mintime) + travel_time
-            problem.emergency_calls[id, delay_col] = total_delay / 60 # minutes
+            problem.emergency_calls[id, :delay] = total_delay / 60 # minutes
             enqueue!(events, (:arrive, id, t + total_delay, amb), t + total_delay)
         end
     else # returned to base location
         returned_to!(redeploy, amb, t)
         returned_to!(problem, stn, t)
-        update_ambulances!(model, stn, 1)
+        update_ambulances!(dispatch, stn, 1)
     end
 end
 
-function simulate_events!(problem::DispatchProblem,
-                          model::DispatchModel,
-                          redeploy::DeployModel,
-                          turnaround::Distributions.LogNormal,
-                          model_name::String="model",
-                          verbose::Bool=false; mini_verbose=false)
+function simulate_events!(
+        problem::DispatchProblem,
+        dispatch::DispatchModel,
+        redeploy::DeployModel,
+        turnaround::Distributions.LogNormal,
+        verbose::Bool=false; mini_verbose=false
+    )
     events = form_queue(problem)
     problem.emergency_calls[:dispatch_from] = 0
     problem.emergency_calls[:delay] = Inf
@@ -158,7 +158,7 @@ function simulate_events!(problem::DispatchProblem,
     while !isempty(events)
         (event, id, t, value) = dequeue!(events)
         if event == :call
-            call_event!(events, problem, model, redeploy, id, t, value)
+            call_event!(events, problem, dispatch, redeploy, id, t, value)
         elseif event == :arrive
             arrive_event!(events, redeploy, id, t, value, turnaround)
         elseif event == :convey
@@ -168,9 +168,9 @@ function simulate_events!(problem::DispatchProblem,
             return_event!(events, problem, redeploy, id, t, value)
         else
             @assert event == :done
-            done_event!(events, problem, model, redeploy, id, t, value)
+            done_event!(events, problem, dispatch, redeploy, id, t, value)
         end
     end
-    @assert all(available .== problem.deployment)
-    @assert all(calls[dispatch_col] .> 0)
+    @assert all(problem.available .== problem.deployment) "$(problem.available), $(problem.deployment)"
+    @assert all(problem.emergency_calls[:dispatch_from] .> 0)
 end
