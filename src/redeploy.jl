e97dd6d77dbb43cebb171c466bbfb0fa050d9b10
yeesian <ngyeesian@gmail.com>
2017-04-21 12:34:58 -0400

    add hospital attribute

--- src/redeploy.jl
+++ src/redeploy.jl
@@ -7,10 +7,11 @@ abstract DeployModel
 
 type NoRedeployModel <: DeployModel
     assignment::Vector{Int} # which location the ambulance is assigned to
-    ambulances::Vector{Vector{Int}}
+    ambulances::Vector{Vector{Int}} # list of ambulances assigned to each location
     status::Vector{Symbol} # the current status of the ambulance
         # possible statuses: :available, :responding, :atscene, :conveying, :returning
     fromtime::Vector{Int} # the time it started the new status
+    hospital::Vector{Int} # the hospital the ambulance is at (0 otherwise)
 end
 
 function NoRedeployModel(p::DeploymentProblem, available::Vector{Int})
@@ -27,7 +28,8 @@ function NoRedeployModel(p::DeploymentProblem, available::Vector{Int})
     @assert sum(length(a) for a in ambulances) == nambulances
     status = fill(:available, nambulances)
     fromtime = zeros(Int, nambulances)
-    NoRedeployModel(assignment, ambulances, status, fromtime)
+    hospital = zeros(Int, nambulances)
+    NoRedeployModel(assignment, ambulances, status, hospital)
 end
 
 reassign_ambulances!(redeploy::DeployModel) = nothing
@@ -46,10 +48,11 @@ function arriveatscene!(redeploy::DeployModel, amb::Int, t::Int)
     redeploy.fromtime[amb] = t
 end
 
-function conveying!(redeploy::DeployModel, amb::Int, t::Int)
+function conveying!(redeploy::DeployModel, amb::Int, hosp::Int, t::Int)
     @assert redeploy.status[amb] == :atscene
-    redeploy.fromtime[amb] = t
     redeploy.status[amb] = :conveying
+    redeploy.fromtime[amb] = t
+    redeploy.hospital[amb] = hosp
 end
 
 function returning_to!(redeploy::DeployModel, amb::Int, t::Int)
@@ -57,11 +60,13 @@ function returning_to!(redeploy::DeployModel, amb::Int, t::Int)
     redeploy.status[amb] = :returning
     redeploy.fromtime[amb] = t
     redeploy.assignment[amb]
+    redeploy.hospital[amb] = 0
 end
 
 function returned_to!(redeploy::DeployModel, amb::Int, t::Int)
     @assert redeploy.status[amb] == :returning
-    redeploy.fromtime[amb] = t
+    @assert redeploy.hospital[amb] == 0
     redeploy.status[amb] = :available
+    redeploy.fromtime[amb] = t
     push!(redeploy.ambulances[redeploy.assignment[amb]], amb)
 end
