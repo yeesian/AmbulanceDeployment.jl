36e886fb7823c03b5d7cbb003b1baabfe2eaec37
yeesian <ngyeesian@gmail.com>
2017-05-09 16:01:03 -0400

    switch to preprocess the emergency calls data

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,31 +6,14 @@ hourly_calls = DataFrames.readtable("data/processed/2-weekday_calls.csv")
 # weekend_hourly_calls = DataFrames.readtable("data/processed/2-weekend_calls.csv")
 adjacent_nbhd = DataFrames.readtable("data/processed/2-adjacent_nbhd.csv")
 coverage = JLD.load("data/processed/3-coverage.jld", "stn_coverage")
-incidents = DataFrames.readtable("data/processed/3-incidents_drivetime.csv");
-hospitals = readtable("data/processed/3-hospitals.csv");
+calls = DataFrames.readtable("data/processed/5-calls.csv");
+hospitals = DataFrames.readtable("data/processed/3-hospitals.csv");
 stations = DataFrames.readtable("data/processed/3-stations.csv");
-
 
 regions = Int[parse(Int,string(x)[2:end]) for x in names(hourly_calls[5:end])]
 locations = collect(1:size(coverage,2))
-hosptals = 1:nrow(hospitals)
 adjacent = convert(Array, adjacent_nbhd[2:end])[regions,regions] .> 0.5
 demand = convert(Array,hourly_calls[:,5:end]);
-
-incidents = incidents[~DataFrames.isna(incidents[:stn1_min]), :] # drop 44 calls that were "unreachable" (because all stations are reachable from each other)
-incidents[:interarrival_seconds] = [0; incidents[:arrival_seconds][2:end] - incidents[:arrival_seconds][1:end-1]]
-
-incidents[:isweekday] = true
-incidents[:isweekday][incidents[:dow] .== "Sun"] = false
-incidents[:isweekday][incidents[:dow] .== "Sat"] = false;
-incidents = incidents[incidents[:isweekday],:];
-
-regions2index = Dict{Int,Int}(regions[i]=>i for i in 1:length(regions))
-incidents[:neighborhood] = [regions2index[x] for x in incidents[:neighborhood]];
-
-calls = incidents[:,[[:hour,:dow,:month,:year,:neighborhood,:interarrival_seconds];
-                     Symbol[Symbol("stn$(i)_min") for i in locations];
-                     Symbol[Symbol("hosp$(i)_min") for i in hosptals]]]
 
 # We focus on emergency calls during the "peak period" (8AM - 8PM),
 # with the emergency calls from the first 3 month as our training set,
@@ -86,6 +69,22 @@ const volatile_turnard = Distributions.LogNormal(3.57, 0.5)
 const steady_turnard = Distributions.LogNormal(3.69, 0.1)
 const stn_names = [Symbol("stn$(i)_min") for i in 1:size(p.coverage,2)];
 
+p = DeploymentProblem(30, length(locations), length(regions), demand, indices[train_filter],
+                                  indices[test_filter], coverage[regions,:], Array{Bool,2}(adjacent))
+turnaround = turnard
+problem = DispatchProblem(calls[inc_test_indices,:], hospitals, stations, p.coverage, turnaround)
+dispatch = ClosestDispatch(p, problem.emergency_calls[:, stn_names])
+problem.emergency_calls[:arrival_seconds] = cumsum(problem.emergency_calls[:interarrival_seconds]);
+name = model_names[1]
+namb = 30
+lambda = 0
+x = amb_deployment[name][namb]
+p.nambulances = namb
+initialize!(problem, x)
+redeploy = AssignmentModel(p, x, hospitals, stations, lambda=Float64(lambda))
+srand(1234) # reset seed
+@time df = simulate_events!(problem, dispatch, redeploy)
+
 # overall simulation
 # @time begin
 #     p = DeploymentProblem(30, length(locations), length(regions), demand, indices[train_filter],
@@ -120,19 +119,3 @@ const stn_names = [Symbol("stn$(i)_min") for i in 1:size(p.coverage,2)];
 #         #DataFrames.writetable("data/processed/4-overall_simulation.csv", problem.emergency_calls)
 #     end
 # end
-
-p = DeploymentProblem(30, length(locations), length(regions), demand, indices[train_filter],
-                                  indices[test_filter], coverage[regions,:], Array{Bool,2}(adjacent))
-turnaround = turnard
-problem = DispatchProblem(calls[inc_test_indices,:], hospitals, stations, p.coverage, turnaround)
-dispatch = ClosestDispatch(p, problem.emergency_calls[:, stn_names])
-problem.emergency_calls[:arrival_seconds] = cumsum(problem.emergency_calls[:interarrival_seconds]);
-name = model_names[1]
-namb = 30
-lambda = 0
-x = amb_deployment[name][namb]
-p.nambulances = namb
-initialize!(problem, x)
-redeploy = AssignmentModel(p, x, hospitals, stations, lambda=Float64(lambda))
-srand(1234) # reset seed
-@time df = simulate_events!(problem, dispatch, redeploy)
