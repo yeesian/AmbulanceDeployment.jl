424be58160c830469f2cd3a786a87a42082f23b4
yeesian <ngyeesian@gmail.com>
2017-05-09 00:06:43 -0400

    code optimizations
    - use Gurobi's API directly (to change coefficients in the constraint
    matrix)
    - provide a solution buffer for reading in solution values
    - reorder the iteration over the solution vector to minimize checks

--- src/redeploy.jl
+++ src/redeploy.jl
@@ -6,11 +6,7 @@ abstract DeployModel
     # fromtime::Vector{Int} # the time it started the new status
 
 type NoRedeployModel <: DeployModel
-    model::JuMP.Model
-    w::Matrix{JuMP.Variable}
-    eta1::Vector{JuMP.Variable}
-    eta2::Matrix{JuMP.Variable}
-    shortfall::Vector{JuMP.ConstraintRef}
+    model::Gurobi.Model
     lambda::Float64
 
     hosp2stn::Matrix{Float64}
@@ -22,6 +18,8 @@ type NoRedeployModel <: DeployModel
         # possible statuses: :available, :responding, :atscene, :conveying, :returning
     fromtime::Vector{Int} # the time it started the new status
     hospital::Vector{Int} # the hospital the ambulance is at (0 otherwise)
+
+    soln::Vector{Float64} # buffer for storing dynamic assignment solutions
 end
 
 function NoRedeployModel(
@@ -30,8 +28,7 @@ function NoRedeployModel(
         # utilization::Vector{Float64},
         hospitals::DataFrame,
         stations::DataFrame;
-        lambda::Float64 = 100.0,
-        solver=GurobiSolver(OutputFlag=0)
+        lambda::Float64 = 100.0
     )
 
     nambulances = sum(available)
@@ -55,61 +52,96 @@ function NoRedeployModel(
     fromtime = zeros(Int, nambulances)
     hospital = zeros(Int, nambulances)
 
-    m = JuMP.Model(solver=solver)
-    JuMP.@variable(m, w[1:nambulances, 1:nlocations], Bin)
-    JuMP.@variable(m, eta1[1:nlocations] >= 0)
-    JuMP.@variable(m, eta2[1:nambulances, 1:nlocations] >= 0)
-    
-    JuMP.@constraint(m, [a=1:nambulances], sum(w[a,i] for i in 1:nlocations) == 1)
-    JuMP.@constraint(m, shortfall[i=1:nlocations], eta1[i] >= available[i] - sum(w[a,i] for a in 1:nambulances))
-    JuMP.@constraint(m, [a=1:nambulances, i=1:nlocations], eta2[a,i] >= w[a,i] - (assignment[a] == i))
-    JuMP.@constraint(m, [a=1:nambulances, i=1:nlocations], eta2[a,i] >= - (w[a,i] - (assignment[a] == i)))
-    
-    JuMP.@objective(m, Min,
-        sum(eta1[i] for i in 1:nlocations) +
-        lambda * sum(eta2[a,i] for a in 1:nambulances, i in 1:nlocations)
-    )
-    JuMP.build(m)
+    m = Gurobi.Model(Gurobi.Env(), "redeploy", :minimize)
+    Gurobi.setparam!(m, "OutputFlag", 0)
+    for a in 1:nambulances, i in 1:nlocations # w variables
+        Gurobi.add_bvar!(m, 0.)
+    end
+    for i in 1:nlocations # eta1
+        Gurobi.add_cvar!(m, 1., 0., Inf)
+    end
+    for a in 1:nambulances, i in 1:nlocations # eta2 variables
+        Gurobi.add_cvar!(m, lambda, 0., Inf)
+    end
 
-    NoRedeployModel(m, w, eta1, eta2, shortfall, lambda, hosp2stn, stn2stn, assignment, ambulances, status, fromtime, hospital)
+    # η >= available[i] - sum(w[a,i] for a in 1:nambulances)
+    #     reformulated to
+    # sum(w[a,i] for a in 1:nambulances) + η >= available[i]
+    for i in 1:nlocations
+        Gurobi.add_constr!(m,
+            [((1:nambulances)-1)*nlocations+i; nambulances*nlocations + i], # inds
+            ones(nambulances + 1), # coeffs
+            '>', Float64(available[i]))
+    end
+    # sum(w[a,i] for i in 1:nlocations) == 1       [a=1:nambulances]
+    for a in 1:nambulances
+        Gurobi.add_constr!(m,
+            collect((a-1)*nlocations + (1:nlocations)), # inds
+            ones(nlocations), # coeffs
+            '=', 1.)
+    end
+
+    # eta2[a,i] >= |w[a,i] - (assignment[a] == i)|   [a=1:nambulances, i=1:nlocations]
+    for a in 1:nambulances, i in 1:nlocations
+        offset = (a-1)*nlocations + i
+        inds = [(nambulances+1)*nlocations + offset, offset]
+        Gurobi.add_constr!(m, inds, [1., -1.], '>', - Float64(assignment[a] == i))
+        Gurobi.add_constr!(m, inds, [1., 1.], '>', Float64(assignment[a] == i))
+    end
+
+    NoRedeployModel(m, lambda, hosp2stn, stn2stn, assignment, ambulances,
+                    status, fromtime, hospital, zeros(nambulances*nlocations))
 end
 
 function reassign_ambulances!(ems, problem::DispatchProblem, redeploy::DeployModel, t::Int)
-    # (1) Optimize Dynamic Assignment Problem
-    cost(a::Int,i::Int) =
-        if redeploy.status[a] == :available
-            redeploy.stn2stn[redeploy.assignment[a],i]
+    nlocations = length(redeploy.ambulances)
+    nambulances = length(redeploy.assignment)
+    # DEBUG
+    # for a in 1:nambulances
+    #     @assert t >= redeploy.fromtime[a]
+    # end
+    cost(a,i) = if redeploy.status[a] == :available
+            0 + redeploy.stn2stn[redeploy.assignment[a],i]
         elseif redeploy.status[a] == :responding
-            60 - (t - redeploy.fromtime[a]) # + redeploy.hosp2stn[redeploy.hospital[a],i]
+            55 - (t - redeploy.fromtime[a])/60 + redeploy.stn2stn[redeploy.assignment[a],i] # + redeploy.hosp2stn[redeploy.hospital[a],i]
         elseif redeploy.status[a] == :atscene
-            45 - (t - redeploy.fromtime[a]) # + redeploy.hosp2stn[redeploy.hospital[a],i]
+            45 - (t - redeploy.fromtime[a])/60 + redeploy.stn2stn[redeploy.assignment[a],i] # + redeploy.hosp2stn[redeploy.hospital[a],i]
         elseif redeploy.status[a] == :conveying
-            30 - (t - redeploy.fromtime[a]) # + redeploy.hosp2stn[redeploy.hospital[a],i]
+            30 - (t - redeploy.fromtime[a])/60 + redeploy.stn2stn[redeploy.assignment[a],i] # + redeploy.hosp2stn[redeploy.hospital[a],i]
         elseif redeploy.status[a] == :returning
-            15 - (t - redeploy.fromtime[a])
+            15 - (t - redeploy.fromtime[a])/60 + redeploy.stn2stn[redeploy.assignment[a],i]
         elseif redeploy.status[a] == :redeploying
-            5 + redeploy.stn2stn[redeploy.assignment[a],i]
+            10 - (t - redeploy.fromtime[a])/60 + redeploy.stn2stn[redeploy.assignment[a],i]
         end
-    
-    for a in 1:size(redeploy.w,1)
-        @assert t >= redeploy.fromtime[a]
+    # (1) Optimize Dynamic Assignment Problem
+    let con = Cint[0], ind = Cint[0], val = Float64[0.0]
+        for i in 1:nlocations, a in 1:nambulances
+            # JuMP.setRHS(s, problem.deployment[loc] + length(problem.wait_queue[loc])) # include backlog from wait_queue
+            con[1] = i
+            ind[1] = (a-1)*nlocations + i
+            val[1] = 1-max(min(60,cost(a,i)),0)/60
+            Gurobi.chg_coeffs!(redeploy.model, con, ind, val)
+        end
     end
-
-    for s in redeploy.shortfall, a in 1:size(redeploy.w,1), i in 1:size(redeploy.w,2)
-        chg_coeffs!(redeploy.model.internalModel.inner, [s.idx], [redeploy.w[a,i].col], [cost(a,i)/60-1])
-    end
-    JuMP.solve(redeploy.model)
+    Gurobi.update_model!(redeploy.model)
+    Gurobi.optimize(redeploy.model)
+    # @show Gurobi.get_objval(redeploy.model)
 
     # (2) Reassign ambulances based on JuMP.getvalue(redeploy.w)
-    for a in 1:size(redeploy.w,1), i in 1:size(redeploy.w,2)
-        if JuMP.getvalue(redeploy.w[a,i]) > 0.5
-            if redeploy.assignment[a] != i # redeploy an existing ambulance
-                if redeploy.status[a] == :available
-                    @assert problem.available[redeploy.assignment[a]] > 0
-                    problem.available[redeploy.assignment[a]] -= 1
-                    t_end = t + ceil(Int, 0*60*redeploy.stn2stn[redeploy.assignment[a],i])
-                    redeploying_to!(redeploy, a, i, t)
-                    enqueue!(ems.eventqueue, (:done, 0, t_end, a), t_end)
+    Gurobi.get_dblattrarray!(redeploy.soln, redeploy.model, "X", 1)
+    # @show [(1-max(min(60,cost(a,i)),0)/60)*soln[redeploy.w[a,i].col] for a in 1:size(redeploy.w,1), i in 1:size(redeploy.w,2)]
+    for a in 1:nambulances
+        if redeploy.status[a] == :available
+            for i in 1:nlocations
+                let stn = redeploy.assignment[a]
+                    # redeploy an existing ambulance
+                    if stn != i && redeploy.soln[(a-1)*nlocations + i] > 0.5
+                        @assert problem.available[redeploy.assignment[a]] > 0
+                        problem.available[stn] -= 1
+                        t_end = t + ceil(Int, 0*60*redeploy.stn2stn[stn,i])
+                        redeploying_to!(redeploy, a, i, t)
+                        enqueue!(ems.eventqueue, (:done, 0, t_end, a), t_end)
+                    end
                 end
             end
         end
@@ -151,6 +183,7 @@ function returning_to!(redeploy::DeployModel, amb::Int, t::Int)
 end
 
 function redeploying_to!(redeploy::DeployModel, amb::Int, i::Int, t::Int)
+    # DEBUG: println("redeploying amb $amb from $(redeploy.assignment[amb]) to $i")
     ambulances = redeploy.ambulances[redeploy.assignment[amb]]
     @assert !in(amb, redeploy.ambulances[i])
     @assert in(amb, ambulances)
