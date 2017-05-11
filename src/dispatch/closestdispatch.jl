ad1ee1717fa5486579a09d6652f29f3b98911031
yeesian <ngyeesian@gmail.com>
2017-05-10 21:41:48 -0400

    - remove unused method `ambulance_for` - `model` -> `dispatch` for naming consistency with `simulate!` - new `ClosestDispatch` constructor

--- src/dispatch/closestdispatch.jl
+++ src/dispatch/closestdispatch.jl
@@ -3,21 +3,6 @@ type ClosestDispatch <: DispatchModel
     candidates::Vector{Vector{Int}}
 end
 
-function ambulance_for(model::ClosestDispatch,
-                       id::Int,
-                       problem::DispatchProblem; verbose=false)
-    i = available_for(model, id, problem)
-    if i == 0
-        verbose && println(problem.available)
-        verbose && println(model.available)
-        return waiting_for(problem.emergency_calls[id, :neighborhood],
-                           problem)
-    else
-        update_ambulances!(model, i, -1)
-        return i
-    end
-end
-
 function ClosestDispatch(p::DeploymentProblem, drivetime::DataFrame)
     candidates = Array(Vector{Int}, p.nregions)
     I = 1:p.nlocations
@@ -27,15 +12,20 @@ function ClosestDispatch(p::DeploymentProblem, drivetime::DataFrame)
     ClosestDispatch(drivetime, candidates)
 end
 
+function ClosestDispatch(p::DeploymentProblem, problem::DispatchProblem)
+    stn_names = [Symbol("stn$(i)_min") for i in 1:size(p.coverage,2)]
+    ClosestDispatch(p, problem.emergency_calls[:, stn_names])
+end
+
 update_ambulances!(model::ClosestDispatch, i::Int, delta::Int) = nothing
 
-function available_for(model::ClosestDispatch, id::Int, problem::DispatchProblem)
+function available_for(dispatch::ClosestDispatch, id::Int, problem::DispatchProblem)
     location = 0
-    min_time = typemax(Int)
-    for i in model.candidates[problem.emergency_calls[id, :neighborhood]]
-        if problem.available[i] > 0 && model.drivetime[id, i] < min_time
+    min_time = Inf
+    for i in dispatch.candidates[problem.emergency_calls[id, :neighborhood]]
+        if problem.available[i] > 0 && dispatch.drivetime[id, i] < min_time
             location = i
-            min_time = model.drivetime[id, i]
+            min_time = dispatch.drivetime[id, i]
         end
     end
     location
